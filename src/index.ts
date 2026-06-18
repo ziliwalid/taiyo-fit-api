2af580b3584638883a188eebf7882171204ceeb3
ziliwalid <walidzili19@gmail.com>
2026-06-18 20:17:15 +0100

    fix(neon): supprimer keep-alive cron + pooler URL + directUrl schema

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import app from './app'
-import { startExpirePaiementsJob } from './jobs/expirePaiements'
 import { startBackupJob } from './jobs/backupJob'
 import { verifyMailer } from './services/mailer'
 
@@ -9,6 +8,5 @@ const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
   console.log(`Taiyo Fit API running on :${PORT}`)
   verifyMailer()
-  startExpirePaiementsJob()
   startBackupJob()
 })
